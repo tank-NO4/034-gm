d191cb4462fa5b0392090f5077368601946fbd18
ZDD-cy <1409104024@qq.com>
2026-03-13 22:51:42 +0800

    重开

---
+++ My project (2)/Assets/Scenes/GameFailManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameFailManager : MonoBehaviour
+{
+    // 单例
+    public static GameFailManager Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // 【核心方法】阶段失败 / 主动重开
+    public void RestartCurrentStage()
+    {
+        // 1. 获取当前正在玩的场景名字
+        string currentSceneName = SceneManager.GetActiveScene().name;
+
+        // 2. 重新加载它，实现完全重置
+        SceneManager.LoadScene(currentSceneName);
+
+        // 🔥 如果你有玩家数据重置需求（如血量、分数），可以在这里写
+        // GameDataManager.Instance.ResetStageData();
+    }
+}
+
---
+++ My project (2)/Assets/Scenes/UIManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    // 重新开始当前关卡
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
+
